b2a28add4dcfc4b906f3977b7423d4bc5403d8ce
carmen-sc <carmensc@student.ethz.ch>
2020-11-12 21:26:11 +0100

    Initial commit of augmented reality AprilTags package, for AMOD CRA1 A-4

--- packages/augmented_reality_apriltag/src/augmented_reality_apriltag.py
+++ packages/augmented_reality_apriltag/src/augmented_reality_apriltag.py
@@ -3,17 +3,20 @@ import numpy as np
 import os
 import math
 import cv2
-from renderClass import Renderer
-
+import copy
 import rospy
+import rospkg 
 import yaml
 import sys
+
+from renderClass import Renderer
 from duckietown.dtros import DTROS, NodeType
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, CameraInfo
+from sensor_msgs.srv import SetCameraInfo, SetCameraInfoResponse
 from cv_bridge import CvBridge, CvBridgeError
 
-import rospkg 
-
+from dt_apriltags import Detector
+from augmented_reality_helper_module import Helpers
 
 """
 
@@ -35,9 +38,49 @@ class ARNode(DTROS):
         # Initialize an instance of Renderer giving the model in input.
         self.renderer = Renderer(rospack.get_path('augmented_reality_apriltag') + '/src/models/duckie.obj')
 
-        #
-        #   Write your code here
-        #
+        # Load calibration files
+        self._res_w = 640
+        self._res_h = 480
+        self.calibration_call()
+
+        # Initialize classes
+        self.at_detec = Detector()
+        self.helper = Helpers(self.current_camera_info)
+        self.bridge = CvBridge()
+
+        # Subscribe to the compressed camera image
+        self.cam_image = rospy.Subscriber(f'/{self.veh}/camera_node/image/compressed', CompressedImage, self.callback, queue_size=10)
+        
+        # Publishers
+        self.aug_image = rospy.Publisher(f'/{self.veh}/{node_name}/image/compressed', CompressedImage, queue_size=10)
+        
+
+    def callback(self, image):
+        """ 
+            Callback method that ties everything together
+        """
+        # convert the image to cv2 format
+        image = self.bridge.compressed_imgmsg_to_cv2(image)
+
+        #undistort the image
+        image = self.helper.process_image(image)
+        
+        # detect AprilTags
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        tags = self.at_detec.detect(gray, estimate_tag_pose=False, camera_params=None, tag_size=None)
+
+        for tag in tags:
+            # estimate homography
+            homography = tag.homography
+
+            # calculate projection matrix
+            proj_mat = self.projection_matrix(self.current_camera_info.K, homography)
+
+            # rendering magics
+            image = self.renderer.render(image, proj_mat)
+
+        #publish image to new topic
+        self.aug_image.publish(self.bridge.cv2_to_compressed_imgmsg(image))
 
 
 
@@ -47,26 +90,26 @@ class ARNode(DTROS):
             Write here the compuatation for the projection matrix, namely the matrix
             that maps the camera reference frame to the AprilTag reference frame.
         """
+        # format the K matrix
+        K = np.reshape(np.array(intrinsic), (3,3))
 
-        #
-        # Write your code here
-        #
+        # calculate R_t
+        R_t = np.matmul(np.linalg.inv(K), homography)
+        
+        # normalization parameters R_t
+        n_r1 = np.linalg.norm(R_t[:, 0])
+        n_r2 = np.linalg.norm(R_t[:, 1])
 
-    def readImage(self, msg_image):
-        """
-            Convert images to OpenCV images
-            Args:
-                msg_image (:obj:`CompressedImage`) the image from the camera node
-            Returns:
-                OpenCV image
-        """
-        try:
-            cv_image = self.bridge.compressed_imgmsg_to_cv2(msg_image)
-            return cv_image
-        except CvBridgeError as e:
-            self.log(e)
-            return []
+        # calculate R_3 and rebuild R_t
+        R = np.array([R_t[:, 0], R_t[:, 1], (n_r1+n_r2)/2*np.cross(R_t[:, 0]/n_r1, R_t[:, 1]/n_r2)])
+        R_t = np.vstack((R, R_t[:, 2]))
 
+        # calculate the projection matrix
+        proj_mat = np.matmul(K, np.transpose(R_t))
+
+        return proj_mat
+
+    
     def readYamlFile(self,fname):
         """
             Reads the 'fname' yaml file and returns a dictionary with its input.
@@ -83,6 +126,95 @@ class ARNode(DTROS):
                          %(fname, exc), type='fatal')
                 rospy.signal_shutdown()
                 return
+
+
+    def calibration_call(self):
+        # copied from the camera driver (dt-duckiebot-interface)
+
+        # For intrinsic calibration
+        self.cali_file_folder = '/data/config/calibrations/camera_intrinsic/'
+        self.frame_id = rospy.get_namespace().strip('/') + '/camera_optical_frame'
+        self.cali_file = self.cali_file_folder + rospy.get_namespace().strip("/") + ".yaml"
+
+        # Locate calibration yaml file or use the default otherwise
+        if not os.path.isfile(self.cali_file):
+            self.logwarn("Calibration not found: %s.\n Using default instead." % self.cali_file)
+            self.cali_file = (self.cali_file_folder + "default.yaml")
+
+        # Shutdown if no calibration file not found
+        if not os.path.isfile(self.cali_file):
+            rospy.signal_shutdown("Found no calibration file ... aborting")
+
+        # Load the calibration file
+        self.original_camera_info = self.load_camera_info(self.cali_file)
+        self.original_camera_info.header.frame_id = self.frame_id
+        self.current_camera_info = copy.deepcopy(self.original_camera_info)
+        self.update_camera_params()
+        self.log("Using calibration file: %s" % self.cali_file)
+
+
+        # extrinsic calibration
+        self.ex_cali_file_folder = '/data/config/calibrations/camera_extrinsic/'
+        self.ex_cali_file = self.ex_cali_file_folder + rospy.get_namespace().strip("/") + ".yaml"
+        self.ex_cali = self.readYamlFile(self.ex_cali_file)
+        
+        # define homography
+        self.homography = self.ex_cali["homography"]
+
+    def update_camera_params(self):
+        """ Update the camera parameters based on the current resolution.
+        The camera matrix, rectification matrix, and projection matrix depend on
+        the resolution of the image.
+        As the calibration has been done at a specific resolution, these matrices need
+        to be adjusted if a different resolution is being used.
+        TODO: Test that this really works.
+        """
+
+        scale_width = float(self._res_w) / self.original_camera_info.width
+        scale_height = float(self._res_h) / self.original_camera_info.height
+
+        scale_matrix = np.ones(9)
+        scale_matrix[0] *= scale_width
+        scale_matrix[2] *= scale_width
+        scale_matrix[4] *= scale_height
+        scale_matrix[5] *= scale_height
+
+        # Adjust the camera matrix resolution
+        self.current_camera_info.height = self._res_h
+        self.current_camera_info.width = self._res_w
+
+        # Adjust the K matrix
+        self.current_camera_info.K = np.array(self.original_camera_info.K) * scale_matrix
+
+        # Adjust the P matrix (done by Rohit)
+        scale_matrix = np.ones(12)
+        scale_matrix[0] *= scale_width
+        scale_matrix[2] *= scale_width
+        scale_matrix[5] *= scale_height
+        scale_matrix[6] *= scale_height
+        self.current_camera_info.P = np.array(self.original_camera_info.P) * scale_matrix
+
+    @staticmethod
+    def load_camera_info(filename):
+        """Loads the camera calibration files.
+        Loads the intrinsic camera calibration.
+        Args:
+            filename (:obj:`str`): filename of calibration files.
+        Returns:
+            :obj:`CameraInfo`: a CameraInfo message object
+        """
+        with open(filename, 'r') as stream:
+            calib_data = yaml.load(stream)
+        cam_info = CameraInfo()
+        cam_info.width = calib_data['image_width']
+        cam_info.height = calib_data['image_height']
+        cam_info.K = calib_data['camera_matrix']['data']
+        cam_info.D = calib_data['distortion_coefficients']['data']
+        cam_info.R = calib_data['rectification_matrix']['data']
+        cam_info.P = calib_data['projection_matrix']['data']
+        cam_info.distortion_model = calib_data['distortion_model']
+        return cam_info
+        
 
 
     def onShutdown(self):
